7939294e04d630a64b56f9891b35c606cbe21f6f
jmbivens <jmbivens03@gmail.com>
2023-09-01 13:54:45 -0400

    added name and email

--- healthPovertyAnalyzer.m
+++ healthPovertyAnalyzer.m
@@ -1,6 +1,7 @@
-% Name(s): Ian Patrick & 
-% Email(s): impatric@ncsu.edu & 
+% Name(s): Ian Patrick & Jessica Bivens
+% Email(s): impatric@ncsu.edu & jmbivens@ncsu.edu
 % Date: 9/4/23
 % Lab Section: 204
 % Project 1: Healthy Poverty Analysis, Fall 2023
 clc ; clear ; close('all');
+
